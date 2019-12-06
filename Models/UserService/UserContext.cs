80c74f91e23fb1e36c66bc238c764a8b80cb0b71
RyanFlachman <ryan@flachman.net>
2019-12-06 11:02:06 -0700

    Add UserContext for UserDb

---
+++ Models/UserService/UserContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using LetsHang.Models;
+
+namespace LetsHang.Models
+{
+  public class UserContext : DbContext
+  {
+    public UserContext(DbContextOptions<UserContext> options)
+    :base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+  }
+}
