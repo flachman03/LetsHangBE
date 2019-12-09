13bbc76d02de4bbc5c184a6ceea11cb5b586b301
RyanFlachman <ryan@flachman.net>
2019-12-09 14:00:42 -0700

    Add User and FriendsList to UserAndFriends Template

--- Models/UserService/Templates/UserAndFriends.cs
+++ Models/UserService/Templates/UserAndFriends.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using LetsHang.Models;
+
 namespace Templates
 {
   public class UserAndFriends
   {
-    
+    [Required]
+    public User User { get; set; }
+
+    [Required]
+    public List<FriendInfo> Friends { get; set; }
   }
 }
